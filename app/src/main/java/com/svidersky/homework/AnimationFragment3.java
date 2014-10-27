fd8be0f795fd9d26e5f9496fb48d79039ec2f2c2
Narutrix <narutrix@mail.ru>
2014-10-28 01:10:10 +0200

    HomeWork2

---
+++ app/src/main/java/com/svidersky/homework/AnimationFragment3.java
@@ -0,0 +1,52 @@
+package com.svidersky.homework;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.svidersky.homework.R;
+
+public class AnimationFragment3 extends Fragment implements Animation.AnimationListener {
+
+    Animation animRotate;
+
+	public AnimationFragment3(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_animation3, container, false);
+        ImageView imgLogo = (ImageView) rootView.findViewById(R.id.imgLogo);
+
+        // load the animation
+        animRotate = AnimationUtils.loadAnimation(getActivity().getApplicationContext(),
+                R.anim.sequential);
+        imgLogo.setVisibility(View.VISIBLE);
+
+        // start the animation
+        imgLogo.startAnimation(animRotate);
+         
+        return rootView;
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
---
+++ app/src/main/java/com/svidersky/homework/AnimationFragment4.java
@@ -0,0 +1,54 @@
+package com.svidersky.homework;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.svidersky.homework.R;
+
+public class AnimationFragment4 extends Fragment implements Animation.AnimationListener {
+
+    Animation animRotate;
+
+	public AnimationFragment4(){}
+
+
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_animation4, container, false);
+        ImageView imgLogo = (ImageView) rootView.findViewById(R.id.imgLogo);
+
+        // load the animation
+        animRotate = AnimationUtils.loadAnimation(getActivity().getApplicationContext(),
+                R.anim.together);
+        imgLogo.setVisibility(View.VISIBLE);
+
+        // start the animation
+        imgLogo.startAnimation(animRotate);
+
+         
+        return rootView;
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
